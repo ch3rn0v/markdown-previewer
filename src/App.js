83a579455e9fdd19c116c809399c25041bdc44d1
Ilya Chernov <ilya.v.chernov@gmail.com>
2017-10-11 00:44:35 +0300

    put header and footer into corresponding files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+import './App.css';
+import { Header } from './Header';
+import { Footer } from './Footer';
+
+class App extends React.Component {
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<h1 className="App-title">Markdown Previewer</h1>
-				</header>
-				<p className="App-intro">TODO: put text area and preview div here.</p>
-				<footer>
-					<p>MIT License.</p>
-					<p>
-						Source code available at{' '}
-						<a
-							href="https://github.com/ch3rn0v/markdown-previewer"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							https://github.com/ch3rn0v/markdown-previewer
-						</a>
-					</p>
-				</footer>
+			<div className="app">
+				<Header />
+				<p className="app-intro">TODO: put text area and preview div here.</p>
+				<Footer />
 			</div>
 		);
 	}
---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Footer = () => {
+	return (
+		<footer>
+			<p>MIT License.</p>
+			<p>
+				Source code available at{' '}
+				<a href="https://github.com/ch3rn0v/markdown-previewer" target="_blank" rel="noopener noreferrer">
+					https://github.com/ch3rn0v/markdown-previewer
+				</a>
+			</p>
+		</footer>
+	);
+};
