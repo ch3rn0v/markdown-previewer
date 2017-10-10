2d6b3357c275f0bf0d1746bcf9998c56eefb0dc4
Ilya Chernov <ilya.v.chernov@gmail.com>
2017-10-11 00:19:43 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">Markdown Previewer</h1>
+				</header>
+				<p className="App-intro">TODO: put text area and preview div here.</p>
+			</div>
+		);
+	}
+}
+
+export default App;
