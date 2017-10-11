3eba846bca89aef521c404ee885f2f75034bcea1
Ilya Chernov <ilya.v.chernov@gmail.com>
2017-10-11 19:57:00 +0300

    update title

--- src/App/Header.js
+++ src/App/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Header = () => {
 	return (
 		<header className="app-header">
-			<h1 className="app-title">Markdown Previewer</h1>
+			<h1 className="app-title">Github Markdown Preview</h1>
 		</header>
 	);
 };
