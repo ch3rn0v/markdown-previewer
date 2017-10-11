87b8f370c29a402cdd1149e51fe383eabc0385d8
Ilya Chernov <ilya.v.chernov@gmail.com>
2017-10-11 19:59:04 +0300

    move disclaimer into footer

--- src/App/Footer.js
+++ src/App/Footer.js
@@ -5,6 +5,12 @@ export const Footer = () => {
 		<footer>
 			<p>MIT License.</p>
 			<p>
+				Disclaimer: this markdown previewer is built with great help of awesome{' '}
+				<a href="https://github.com/markdown-it/markdown-it" target="_blank" rel="noopener noreferrer">
+					markdown-it library
+				</a>.
+			</p>
+			<p>
 				Source code available at{' '}
 				<a href="https://github.com/ch3rn0v/markdown-previewer" target="_blank" rel="noopener noreferrer">
 					https://github.com/ch3rn0v/markdown-previewer
